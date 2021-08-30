ccacc6273b77691eeb3b5055239853a169dc2352
1chrisf <3chrisf@gmail.com>
2021-08-30 20:13:15 +0100

    updated gui

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,69 +1,58 @@
 clearBtn = document.getElementById('clear');
-clearBtn.addEventListener('click', clearGrid);
-let slider = document.getElementById('gridSize');
+clearBtn.addEventListener('click', resetGrid);
+colorBtn = document.getElementById('color');
+colorBtn.addEventListener('click', colorSwitch);
+const slider = document.getElementById('gridSize');
 slider.onchange = (e) => newGrid(e.target.value);
+const grid = document.getElementById('grid');
 gridSize = 24;//squared
-function start() {
-    const grid = document.getElementById('grid');
-    grid.style['grid-template-columns'] = `repeat(${gridSize}, minmax(1px, 1fr))`;
-    grid.style['grid-template-rows'] = `repeat(${gridSize}, minmax(1fr, 1fr))`;
-}
-start();
-
+let color = false;
 createGrid();
 
-
-function gridUpdate() {
-    const grid = document.getElementById('grid');
-    grid.style['grid-template-columns'] = `repeat(${gridSize}, minmax(1px, 1fr))`;
-    grid.style['grid-template-rows'] = `repeat(${gridSize}, minmax(1fr, 1fr))`;
-    createGrid();
+function colorSwitch() {
+    (color) ? color = false : color = true;
 }
 function createGrid() {
-
+    grid.style['grid-template-columns'] = `repeat(${gridSize}, minmax(1px, 1fr))`;
+    grid.style['grid-template-rows'] = `repeat(${gridSize}, minmax(1fr, 1fr))`;
     for (i = 0; i < gridSize * gridSize; ++i) {
-
         square = document.createElement('div');
         square.id = `square${i}`;
         console.log(square)
-        num1 = Math.floor(Math.random() * 111);
-        num2 = Math.floor(Math.random() * 33);
-        num3 = Math.floor(Math.random() * 222);
-        num4 = Math.floor(Math.random() * ((6 - 2) + 2)) / 10;
-        console.log(num4)
-        //square.style['background-color'] = `rgba(${num1},${num2},${num3},${num4})`
-        square.style['background-color'] = `rgba(100,100,100,${num4})`
         grid.appendChild(square)
-        //addMouseEnter();
-        let hover = document.getElementById(`square${i}`);
-
-        hover.addEventListener('mouseenter', draw);
+        a = document.getElementById(`square${i}`)
+        a.addEventListener('mouseenter', draw);
+        a.classList.add('square');
     }
+    resetGrid();
 }
-function draw() {
-    // this.style['background-color'] = `rgba(15, 15, 15, 0.705)`;
-    this.style['background-image'] = 'linear-gradient(180deg, rgba(20, 20, 20, 0.705), rgba(15, 15, 15, 0.2))';
-}
-
 function newGrid(value) {
-    for (i = 0; i < gridSize * gridSize; ++i) {
-        square2 = document.getElementById(`square${i}`);
-        if (square2 === null) return;
-        square2.remove(`square${i}`);
-
-    }
+    let a = document.querySelectorAll('.square');
+    a.forEach(item => grid.removeChild(item));
     gridSize = value;
-    gridUpdate();
+    createGrid();
+    //createGrid();
 }
-function clearGrid(value) {
+function resetGrid() {
     for (i = 0; i < gridSize * gridSize; ++i) {
-        square2 = document.getElementById(`square${i}`);
-        if (square2 === null) return;
-        square2.remove(`square${i}`);
-
+        let square = document.getElementById(`square${i}`);
+        num4 = Math.floor(Math.random() * ((5 - 1) + 1)) / 10;
+        square.classList.remove("col");
+        square.removeAttribute("style");
+        square.style['background-color'] = `rgba(120,120,120,${num4})`
     }
-
-    gridUpdate();
 }
-
+function draw() {    
+    if (color) {
+        if (this.classList.value === 'square col') { return };
+        num1 = Math.floor(Math.random() * 255);//for random color.
+        num2 = Math.floor(Math.random() * 255);
+        num3 = Math.floor(Math.random() * 100);
+        this.classList.add('col');
+        console.log(this)
+        this.style['background-color'] = `rgba(${num1},${num2},${num3}, 0.8)`
+    } else {
+        this.style['background-image'] = 'linear-gradient(180deg, rgba(15, 15, 15, 0.8), rgba(15, 15, 15, 0.2))';
+    }
+}
 
